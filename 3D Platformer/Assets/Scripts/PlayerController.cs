a5bbe21149567cd0ff2b2b504d13efd998b56117
grahamwilk <97996848+grahamwilk@users.noreply.github.com>
2022-10-05 13:48:24 -0500

    in progress of better movement

--- 3D Platformer/Assets/Scripts/PlayerController.cs
+++ 3D Platformer/Assets/Scripts/PlayerController.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
 
 
     public float moveSpeed;
-    public Rigidbody theRB;
+    //public Rigidbody theRB;
+    public float jumpForce;
+    public CharacterController controller;
+    private Vector3 moveDirection;
 
     // Start is called before the first frame update
     void Start()
     {
-        theRB = GetComponent<Rigidbody>();
+        //theRB = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        /*theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed,theRB.velocity.y,Input.GetAxis("Vertical") * moveSpeed);
+        if (Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector3(theRB.velocity.x,jumpForce,theRB.velocity.y);
+        }*/
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
