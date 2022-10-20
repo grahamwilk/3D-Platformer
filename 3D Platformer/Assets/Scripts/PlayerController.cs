afa0adb8514201513593e5b4a69f7a75eddff0c8
grahamwilk <97996848+grahamwilk@users.noreply.github.com>
2022-10-20 01:16:10 -0500

    Code through video 7
    Added basic camera controls + improved movement of the player

--- 3D Platformer/Assets/Scripts/PlayerController.cs
+++ 3D Platformer/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
 
 
     public float moveSpeed;
-    //public Rigidbody theRB;
     public float jumpForce;
     public CharacterController controller;
     private Vector3 moveDirection;
@@ -17,28 +16,26 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //theRB = GetComponent<Rigidbody>();
         controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed,theRB.velocity.y,Input.GetAxis("Vertical") * moveSpeed);
-        if (Input.GetButtonDown("Jump"))
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
+        if (controller.isGrounded)
         {
-            theRB.velocity = new Vector3(theRB.velocity.x,jumpForce,theRB.velocity.y);
-        }*/
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (Physics.Raycast(transform.position, Vector3.down, 1.1f))
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump"))
             {
                 moveDirection.y = jumpForce;
             }
+
         }
-        //if (Physics.Raycast(transform.position, Vector3.down, 1.1f))
-        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
         controller.Move(moveDirection * Time.deltaTime);
     }
 }
