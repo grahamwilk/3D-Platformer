ba1c7ddffbea22df50eb0c1b17adcb3e395ec13e
alowell611 <alowell611@gmail.com>
2022-11-03 15:31:24 -0500

    Character model and animations

--- 3D Platformer/Assets/Scripts/PlayerController.cs
+++ 3D Platformer/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-
-
     public float moveSpeed;
     //public Rigidbody theRB;
     public float jumpForce;
@@ -19,20 +16,15 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //theRB = GetComponent<Rigidbody>();
         controller = GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed,theRB.velocity.y,Input.GetAxis("Vertical") * moveSpeed);
-        if (Input.GetButtonDown("Jump"))
-        {
-            theRB.velocity = new Vector3(theRB.velocity.x,jumpForce,theRB.velocity.y);
-        }*/
         moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
-    
+
         if (controller.isGrounded)
         {
             if (Input.GetButtonDown("Jump"))
@@ -42,11 +34,12 @@ public class PlayerController : MonoBehaviour
                     moveDirection.y = jumpForce;
                 }
             }
+
         }
-        //if (Physics.Raycast(transform.position, Vector3.down, 1.1f))
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
         controller.Move(moveDirection * Time.deltaTime);
         anim.SetBool("isGrounded", controller.isGrounded);
         anim.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
     }
 }
+
